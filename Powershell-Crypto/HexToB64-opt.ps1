94a886c2e592a47b20edf13c4ad0b5b41b2f9371
Peter Marquis <EnviableOne@users.noreply.github.com>
2019-04-04 16:31:11 +0100

    Add files via upload

---
+++ Powershell-Crypto/HexToB64-opt.ps1
@@ -0,0 +1,12 @@
+﻿Function Convert-HexToBase64
+{
+ param(
+  [parameter(Mandatory=$true)][string]$HexString
+  )
+ $SrcByte = [byte[]]::new(20);
+ for ($i=0;$i -lt $HexString.Length; $i+=2){
+  $SrcByte[$i/2] = [convert]::ToByte($HexString.Substring($i,2),16);
+ }
+ $OutStr = [convert]::ToBase64String($SrcByte); 
+ $OutStr;
+}
---
+++ Powershell-Crypto/HexToB64.ps1
@@ -0,0 +1,28 @@
+﻿Function Convert-HexToBase64
+{
+ param(
+  [parameter(Mandatory=$true)][string]$HexString
+  )
+ $SrcByte = [byte[]]::new(20);
+ for ($i=0;$i -lt $HexString.Length; $i+=2){
+  $SrcByte[$i/2] = [convert]::ToByte($HexString.Substring($i,2),16);
+ }
+ $OutStr = [convert]::ToBase64String($SrcByte); 
+ $OutStr;
+}
+Function Convert-Base64ToHex {
+ param(
+  [parameter(Mandatory=$true)][String]$Base64Str
+ )
+ If ($SrcType="STR"){
+  $SrcByte = [convert]::FromBase64String($Base64Str);
+ }
+ Else{
+  $SrcByte = $Base64Str;
+ }
+ $HexString = [System.Text.StringBuilder]::new($SrcByte.Length * 2);
+ ForEach($byte in $SrcByte){
+  $HexString.AppendFormat("{0:x2}", $byte) | Out-Null;
+ }
+ $HexString.ToString();
+}
