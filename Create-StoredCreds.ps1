dd41fa585cee41a22108ff37715eb355de6be320
Peter Marquis <EnviableOne@users.noreply.github.com>
2020-09-07 18:14:49 +0100

    Add files via upload

---
+++ Create-StoredCreds.ps1
@@ -0,0 +1,8 @@
+﻿$ErrorActionPreference = "Stop"
+Import-Module C:\EnviFun\ad\TestCred.ps1
+$Creds = Get-Credential
+while(!(Test-Cred $Creds)){
+ $Creds = Get-Credential
+}
+$SecureStringText = $Creds.Password | ConvertFrom-SecureString
+Set-Content "C:\audit\pwdaudit\includes\SpongeyAdm.txt" $SecureStringText
---
+++ Get-StoredCreds.ps1
@@ -0,0 +1,6 @@
+﻿$user = "marp0201"
+$pwdString = Get-Content "C:\audit\pwdaudit\includes\spongecake.txt"
+$Credpwd = $pwdString | ConvertTo-SecureString
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $User,$Credpwd
+
+Test-Cred $cred
